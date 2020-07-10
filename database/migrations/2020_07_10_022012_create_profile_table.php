9913a2edf2c6ab60c54443acd42e5a45ec9461bc
VietAnh Nguyen <vietanhnguyen31@gmail.com>
2020-07-10 10:55:56 +0700

    Feat(webapp): Create Database Migrations

---
+++ database/migrations/2020_07_10_022012_create_profile_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 60)->nullable();
+            $table->string('last_name', 60)->nullable();
+            $table->string('image_avatar')->nullable();
+            $table->string('phone', 20);
+            $table->string('address_line_1', 100)->nullable();
+            $table->string('address_line_2', 100)->nullable();
+            $table->string('city');
+            $table->string('country');
+            $table->integer('person_id');
+            $table->string('person_type', 60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
---
+++ database/migrations/2020_07_10_023028_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->smallInteger('id', true, true);
+            $table->string('name', 60);
+            $table->string('slug', 60);
+            $table->smallInteger('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
